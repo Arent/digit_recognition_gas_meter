5be5a80f7de285b3011293a64a6d45176fea0200
Arent Stienstra <arentstienstra@Arents-MacBook-Air.local>
2022-12-30 19:15:01 +0100

    Log the image with the prediction, remove the old one

--- stadswarmte_sensor/main.py
+++ stadswarmte_sensor/main.py
@@ -1,10 +1,19 @@
 import datetime
+import os
 import time
 from pathlib import Path
 
 from PIL import Image
 
 from stadswarmte_sensor import app_settings, camera, mqqt, segment_recognition
+
+
+def log_to_disk(
+    image: Image.Image, digits: list[int], timestamp: str, base_path: Path
+) -> None:
+    digits_string = "".join([str(d) for d in digits])
+    name = f"{timestamp}__{digits_string}.png"
+    image.save(base_path / name)
 
 
 def capture_recognise_and_publish(settings: app_settings.AppSettings):
@@ -17,12 +26,17 @@ def capture_recognise_and_publish(settings: app_settings.AppSettings):
 
     timestamp = datetime.datetime.utcnow().strftime("%Y_%m_%d_%H_%M_%S")
 
-    name = f"{settings.images_folder}/{timestamp}__no_prediction.jpg"
+    name = f"{settings.images_folder}/{timestamp}__no_prediction.png"
 
     image = camera.capture_image(settings.camera_settings, name)
     digits = segment_recognition.process_and_predict(
         image, settings.digit_recognition_settings
     )
+
+    log_to_disk(image, digits, timestamp, settings.images_folder)
+
+    os.remove(name)
+
     mqqt.publish_message(digits, timestamp, settings.mqqt_settings)
 
 
